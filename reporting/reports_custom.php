3525aefb2eee62effeeca174e63abf2a073bacda
Maxime Bourget <bmx007@gmail.com>
2016-06-08 15:29:07 +0100

    Change begin date to today-6month

--- reporting/reports_custom.php
+++ reporting/reports_custom.php
@@ -3,7 +3,7 @@
 global $reports;
 
 $reports->addReport(RC_CUSTOMER,"_customer_account_statement",_('Customer Account Statement'),
-	array(_('Start Date') => 'DATEBEGIN',
+	array(_('Start Date') => 'DATEMINUS6',
 				('End Date') => 'DATE',
 			_('Customer') => 'CUSTOMERS_NO_FILTER',
 			_('Currency Filter') => 'CURRENCY',
